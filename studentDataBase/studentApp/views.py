ecac4274d78edd0d0889d083b11efaf522f2ee7e
islamr <r.islam@innosep.de>
2023-06-07 18:30:38 +0200

    create table for saving and showing data in DB

--- studentDataBase/studentApp/views.py
+++ studentDataBase/studentApp/views.py
@@ -2,17 +2,16 @@ from django.shortcuts import render
 
 
 def index(request):
-
     return render(request, 'index.html')
+
 
 def insertStudentData(request):
     if request.method == 'POST':
         name = request.POST.get('name')
         email = request.POST.get('email')
-        age = request.POST.get('age')
-        gender = request.POST.get('gender')
+        age = request.POST['age']
+        gender = request.POST['gender']
 
-        print(name,email, age, gender)
-
+        print(name, email, age, gender)
 
     return render(request, 'index.html')
