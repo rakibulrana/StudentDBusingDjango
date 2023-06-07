694578496cd0bd5480b9cf547fa0e82bdbad3625
islamr <r.islam@innosep.de>
2023-06-07 15:56:50 +0200

    received font end data to backend using insertStudentData method

--- studentDataBase/studentApp/urls.py
+++ studentDataBase/studentApp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from studentApp import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('insert', views.insertStudentData, name='insertStudentData'),
 ]
--- studentDataBase/studentApp/views.py
+++ studentDataBase/studentApp/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 def index(request):
 
     return render(request, 'index.html')
+
+def insertStudentData(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        age = request.POST.get('age')
+        gender = request.POST.get('gender')
+
+        print(name,email, age, gender)
+
+
+    return render(request, 'index.html')
